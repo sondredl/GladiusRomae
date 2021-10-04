dfdb7ba5f7304fca201535710390918881d37a38
Sondre Lindkjølen <47851430+sondredl@users.noreply.github.com>
2021-10-04 09:50:56 +0200

    fixing pause menu on/off

--- Good_Game/Assets/SampleScenes/Menu/Scripts/PauseMenu.cs
+++ Good_Game/Assets/SampleScenes/Menu/Scripts/PauseMenu.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+     public void Resume ()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    
+    void Pause ()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading menu...");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+    }
+
+
     private Toggle m_MenuToggle;
 	private float m_TimeScaleRef = 1f;
     private float m_VolumeRef = 1f;
@@ -49,15 +93,15 @@ public class PauseMenu : MonoBehaviour
     }
 
 
-#if !MOBILE_INPUT
-	void Update()
-	{
-		if(Input.GetKeyUp(KeyCode.Escape))
-		{
-		    m_MenuToggle.isOn = !m_MenuToggle.isOn;
-            Cursor.visible = m_MenuToggle.isOn;//force the cursor visible if anythign had hidden it
-		}
-	}
-#endif
+//#if !MOBILE_INPUT
+//	void Update()
+//	{
+//		if(Input.GetKeyUp(KeyCode.Escape))
+//		{
+//		    m_MenuToggle.isOn = !m_MenuToggle.isOn;
+//            Cursor.visible = m_MenuToggle.isOn;//force the cursor visible if anythign had hidden it
+//		}
+//	}
+//#endif
 
 }
