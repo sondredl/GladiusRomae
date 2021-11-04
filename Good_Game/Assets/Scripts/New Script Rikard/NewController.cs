28eb053d668e09f0d0f38f2304344dfdbed35ba8
Rikard Rasmussen <rikardwrasmussen@outlook.com>
2021-11-04 14:07:35 +0100

    Revert "1"
    This reverts commit 7b27046791f9f4557711fd7b1959fc1f28ab6380.
    
    updated
    
    minimize size
    
    1

---
+++ Good_Game/Assets/Scripts/New Script Rikard/NewController.cs
@@ -0,0 +1,254 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+
+[RequireComponent(typeof(CharacterController))]
+#if ENABLE_INPUT_SYSTEM
+
+[RequireComponent(typeof(PlayerInput))]
+#endif
+
+public class NewController : MonoBehaviour
+{
+	[Header("Player")]
+	[Tooltip("Move speed of the character in m/s")]
+	public float MoveSpeed = 2.0f;
+	[Tooltip("Sprint speed of the character in m/s")]
+	public float SprintSpeed = 5.335f;
+	[Tooltip("How fast the character turns to face movement direction")]
+	[Range(0.0f, 0.3f)]
+	public float RotationSmoothTime = 0.12f;
+	[Tooltip("Acceleration and deceleration")]
+	public float SpeedChangeRate = 10.0f;
+
+	[Space(10)]
+	[Tooltip("The height the player can jump")]
+	public float JumpHeight = 1.2f;
+	[Tooltip("The character uses its own gravity value. The engine default is -9.81f")]
+	public float Gravity = -15.0f;
+
+
+
+	[Space(10)]
+	[Tooltip("Time required to pass before being able to jump again. Set to 0f to instantly jump again")]
+	public float JumpTimeout = 0.50f;
+	public float AttackTimeOut = 3.7f;
+
+	[Header("Cinemachine")]
+	[Tooltip("The follow target set in the Cinemachine Virtual Camera that the camera will follow")]
+	public GameObject CinemachineCameraTarget;
+	[Tooltip("How far in degrees can you move the camera up")]
+	public float TopClamp = 70.0f;
+	[Tooltip("How far in degrees can you move the camera down")]
+	public float BottomClamp = -30.0f;
+	[Tooltip("Additional degress to override the camera. Useful for fine tuning camera position when locked")]
+	public float CameraAngleOverride = 0.0f;
+	[Tooltip("For locking the camera position on all axis")]
+	public bool LockCameraPosition = false;
+
+
+
+	// cinemachine
+	private float _cinemachineTargetYaw;
+	private float _cinemachineTargetPitch;
+
+	// player
+	private float _speed;
+	private float _animationBlend;
+	private float _targetRotation = 0.0f;
+	private float _rotationVelocity;
+	private float _verticalVelocity;
+	private float _terminalVelocity = 53.0f;
+
+	// timeout deltatime
+	private float _jumpTimeoutDelta;
+	private float _attackTimeOutDelta;
+
+	[Tooltip("player is attacking")]
+	public bool attacking = false;
+
+	[Tooltip("player is jumping")]
+	public bool jump = false;
+
+
+	private Animator animator;
+	private CharacterController controller;
+	private NewInput input;
+	private GameObject mainCamera;
+
+	private const float threshold = 0.01f;
+
+	private bool hasAnimator;
+
+	private void Awake()
+	{
+		// get a reference to our main camera
+		if (mainCamera == null)
+		{
+			mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+		}
+	}
+
+
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		hasAnimator = TryGetComponent(out animator);
+		controller = GetComponent<CharacterController>();
+		input = GetComponent<NewInput>();
+
+
+		// reset our timeouts on start
+		_attackTimeOutDelta = AttackTimeOut;
+		_jumpTimeoutDelta = JumpTimeout;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		hasAnimator = TryGetComponent(out animator);
+		Taunt();
+		Move();
+	}
+
+
+
+	private void LateUpdate()
+	{
+		CameraRotation();
+	}
+
+	private void Taunt()
+	{
+
+
+		if (!attacking)
+		{
+			if (input.attack)
+			{
+				animator.SetTrigger("Attack");
+				attacking = true;
+
+			}
+		}
+
+		if (!jump)
+		{
+			if (input.jump)
+			{
+				animator.SetTrigger("Jump");
+				jump = true;
+
+			}
+		}
+
+		else
+		{
+			_attackTimeOutDelta -= Time.deltaTime;
+			input.attack = false;
+		}
+		if (_attackTimeOutDelta <= 0)
+		{
+			_attackTimeOutDelta = AttackTimeOut;
+			attacking = false;
+		}
+
+		//input.attack = false;
+
+	}
+
+	private void Move()
+	{
+		// set target speed based on move speed, sprint speed and if sprint is pressed
+		float targetSpeed = input.sprint ? SprintSpeed : MoveSpeed;
+
+		// a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
+
+		// note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
+		// if there is no input, set the target speed to 0
+		if (input.move == Vector2.zero) targetSpeed = 0.0f;
+
+		// a reference to the players current horizontal velocity
+		float currentHorizontalSpeed = new Vector3(controller.velocity.x, 0.0f, controller.velocity.z).magnitude;
+
+		float speedOffset = 0.1f;
+		float inputMagnitude = input.analogMovement ? input.move.magnitude : 1f;
+
+		// accelerate or decelerate to target speed
+		if (currentHorizontalSpeed < targetSpeed - speedOffset || currentHorizontalSpeed > targetSpeed + speedOffset)
+		{
+			// creates curved result rather than a linear one giving a more organic speed change
+			// note T in Lerp is clamped, so we don't need to clamp our speed
+			_speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude, Time.deltaTime * SpeedChangeRate);
+
+			// round speed to 3 decimal places
+			_speed = Mathf.Round(_speed * 1000f) / 1000f;
+		}
+		else
+		{
+			_speed = targetSpeed;
+		}
+		_animationBlend = Mathf.Lerp(_animationBlend, targetSpeed, Time.deltaTime * SpeedChangeRate);
+
+		// normalise input direction
+		Vector3 inputDirection = new Vector3(input.move.x, 0.0f, input.move.y).normalized;
+
+		// note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
+		// if there is a move input rotate player when the player is moving
+		if (input.move != Vector2.zero)
+		{
+			_targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + mainCamera.transform.eulerAngles.y;
+			float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity, RotationSmoothTime);
+
+			// rotate to face input direction relative to camera position
+			transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+		}
+
+
+		Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
+
+		// move the player
+		controller.Move(targetDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+
+		// update animator if using character
+		if (hasAnimator)
+		{
+			animator.SetFloat("Speed", _animationBlend);
+			/*animator.SetFloat("MoveSpeed",animIDMotionSpeed, inputMagnitude);*/
+		}
+	}
+
+
+	private void CameraRotation()
+	{
+		// if there is an input and camera position is not fixed
+		if (input.look.sqrMagnitude >= threshold && !LockCameraPosition)
+		{
+			_cinemachineTargetYaw += input.look.x * Time.deltaTime;
+			_cinemachineTargetPitch += input.look.y * Time.deltaTime;
+		}
+
+		// clamp our rotations so our values are limited 360 degrees
+		_cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
+		_cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
+
+		// Cinemachine will follow this target
+		CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride, _cinemachineTargetYaw, 0.0f);
+	}
+
+
+
+	private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
+	{
+		if (lfAngle < -360f) lfAngle += 360f;
+		if (lfAngle > 360f) lfAngle -= 360f;
+		return Mathf.Clamp(lfAngle, lfMin, lfMax);
+	}
+
+}
---
+++ Good_Game/Assets/Scripts/New Script Rikard/NewInput.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewInput : MonoBehaviour
+{
+    [Header("Character Input Values")]
+    public Vector2 move;
+    public Vector2 look;
+    public bool jump;
+    public bool sprint;
+    public bool attack;
+
+    [Header("Movement Settings")]
+    public bool analogMovement;
+
+#if !UNITY_IOS || !UNITY_ANDROID
+    [Header("Mouse Cursor Settings")]
+    public bool cursorLocked = true;
+    public bool cursorInputForLook = true;
+#endif
+
+    public void OnMove(InputValue value)
+    {
+        MoveInput(value.Get<Vector2>());
+    }
+
+    public void OnLook(InputValue value)
+    {
+        if (cursorInputForLook)
+        {
+            LookInput(value.Get<Vector2>());
+        }
+    }
+
+    public void OnJump(InputValue value)
+    {
+        JumpInput(value.isPressed);
+    }
+
+    public void OnSprint(InputValue value)
+    {
+        SprintInput(value.isPressed);
+    }
+
+    public void OnAttack(InputValue value)
+    {
+        attackInput(value.isPressed);
+    }
+
+
+    public void MoveInput(Vector2 newMoveDirection)
+    {
+        move = newMoveDirection;
+    }
+
+    public void LookInput(Vector2 newLookDirection)
+    {
+        look = newLookDirection;
+    }
+
+    public void JumpInput(bool newJumpState)
+    {
+        jump = newJumpState;
+    }
+
+    public void SprintInput(bool newSprintState)
+    {
+        sprint = newSprintState;
+    }
+
+    public void attackInput(bool newAttackState)
+    {
+        attack = newAttackState;
+    }
+}
