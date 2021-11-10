ffc671a9656dd905c935ce6ceef7c79232f1c5df
Sondre Lindkjølen <47851430+sondredl@users.noreply.github.com>
2021-11-10 16:04:25 +0100

    add left and right mouse attack

--- Good_Game/Assets/PlayerMovement.cs
+++ Good_Game/Assets/PlayerMovement.cs
@@ -6,31 +6,74 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    private CharacterController  characterController;
+    private CharacterController characterController;
+    public Transform cam;
     private Animator animator;
 
-    [SerializeField]
-    private float moveSpeed = 100;
-    [SerializeField]
-    private float turnSpeed = 5;
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float backwardSpeed = 5f;
+    [SerializeField] private float turnSpeed = 150;
+    // [SerializeField] bool m_Crouch = false;
+    // public float turnSmoothTime = 0.1f;
 
-    private void Awake () {
+    private void Awake()
+    {
         characterController = GetComponent<CharacterController>();
         animator = GetComponentInChildren<Animator>();
     }
-    private void Update () {
+    private void Update()
+    {
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
+        var movement = new Vector3(horizontal, 0, vertical).normalized;
 
-        var movement = new Vector3(horizontal, 0, vertical);
+        // forward run animation
+        animator.SetFloat("Speed", vertical);
+        transform.Rotate(Vector3.up, horizontal * turnSpeed * Time.deltaTime);
 
-        characterController.SimpleMove(movement * Time.deltaTime * moveSpeed);
-
-        animator.SetFloat("Speed", movement.magnitude);
-
-        if (movement.magnitude > 0) {
-            Quaternion newDirection = Quaternion.LookRotation(movement);
-            transform.rotation = Quaternion.Slerp(transform.rotation, newDirection, Time.deltaTime * turnSpeed);
+        if (vertical != 0)
+        {
+            characterController.SimpleMove(transform.forward * moveSpeed * vertical);
         }
+
+        // left and right mouse click attack
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("Attack");
+            Debug.Log("attack trigger engaged");
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            animator.SetTrigger("Attack2");
+            Debug.Log("attack 2 trigger engaged");
+        }
+
+        // animator.SetTrigger("Jump");
+        // transform.Rotate(Vector3.up, horizontal * turnSpeed * Time.deltaTime);
+
+        // backward run animation
+        // animator.SetFloat("Speed", vertical);
+        // transform.Rotate(Vector3.down, horizontal * turnSpeed * Time.deltaTime);
+
+        // var attack = new bool();
+
+        // characterController.SimpleMove(movement * Time.deltaTime * moveSpeed);
+        // // characterController(attack);
+
+        // animator.SetFloat("Speed", movement.magnitude);
+        // // animator.SetBool("Fire1", attack);
+
+        // if (movement.magnitude > 0)
+        // {
+        //     Quaternion newDirection = Quaternion.LookRotation(movement);
+        //     transform.rotation = Quaternion.Slerp(transform.rotation, newDirection, Time.deltaTime * turnSpeed);
+
+        //     // float targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        //     // float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSpeed, turnSmoothTime);
+        //     // transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+        //     // Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+        //     // characterController.Move(moveDir.normalized * moveSpeed * Time.deltaTime);
+        // }
     }
 }
--- Good_Game/Assets/ThirdPersonMovement.cs
+++ Good_Game/Assets/ThirdPersonMovement.cs
@@ -6,10 +6,16 @@ public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
+    private Animator animator;
     public float speed = 6f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -17,7 +23,12 @@ public class ThirdPersonMovement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if(direction.magnitude >= 0.1f) {
+        controller.SimpleMove(direction * Time.deltaTime * speed);
+
+        animator.SetFloat("Speed", direction.magnitude);
+
+        if (direction.magnitude >= 0.1f)
+        {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
@@ -25,6 +36,6 @@ public class ThirdPersonMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
-        
+
     }
 }
