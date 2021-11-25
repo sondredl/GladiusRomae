b3401f764a44435c3836ccdf1a5df772180d9370
Sondre Lindkjølen <47851430+sondredl@users.noreply.github.com>
2021-11-25 16:40:39 +0100

    add mainMenu.cs to scene

--- Good_Game/Assets/Scripts/MainMenu.cs
+++ Good_Game/Assets/Scripts/MainMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu1 : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
 
 //    public GameObject pauseMenuUI;
