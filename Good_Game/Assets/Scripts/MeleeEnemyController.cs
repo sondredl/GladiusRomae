102af8a90f4d29271a2a8f69b5832aeadd1fb3e9
Sondre Lindkjølen <47851430+sondredl@users.noreply.github.com>
2021-11-28 13:15:41 +0100

    enemy animation blend tree
    animation does not stop when speed is 0

--- Good_Game/Assets/Scripts/MeleeEnemyController.cs
+++ Good_Game/Assets/Scripts/MeleeEnemyController.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(CharacterController))]
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+
+[RequireComponent(typeof(NavMeshAgent))]
+#endif
+
 public class MeleeEnemyController : MonoBehaviour
 {
+    // public NavMeshAgent navMeshAgent;
     public float lookRadius = 10f;
+	private float _speed = 10;
+	private float _animationBlend;
 
     public static bool isAlive;
     public int maxHealth = 100;
@@ -53,6 +62,7 @@ public class MeleeEnemyController : MonoBehaviour
             agent.SetDestination(target.position);
             // Debug.Log("target position: " + target.position);
             FaceTarget();
+            Move();
 
             if (distance <= agent.stoppingDistance) {
                 // attack the target
@@ -114,4 +124,83 @@ public class MeleeEnemyController : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, lookRadius);
     }
+
+	private void Move()
+	{
+		float currentHorizontalSpeed = new Vector3(controller.velocity.x, 0.0f, controller.velocity.z).magnitude;
+        // Debug.Log("horizontal speed: " + currentHorizontalSpeed);
+		float speedOffset = 9f;
+        
+        // _speed = agent.speed;
+        _animationBlend = agent.speed;
+        Debug.Log("navMesh speed: " + _animationBlend);
+
+        if (agent.speed > 1) {
+            // Debug.Log("_animationBlend if: " + _animationBlend);
+			enemyAnimator.SetFloat("Speed", _animationBlend);
+        }
+        else {
+            Debug.Log("_animationBlend else: " + _animationBlend);
+			// enemyAnimator.SetFloat("Speed", _animationBlend);
+            enemyAnimator.Play("motion");
+        }
+
+		// float speedOffset = 1f;
+		// float speedOffset = 9f;
+
+		// if (hasAnimator)
+		// {
+		// 	enemyAnimator.SetFloat("Speed", _animationBlend);
+		// 	// animator.SetFloat("MoveSpeed",animIDMotionSpeed, inputMagnitude);
+		// }
+		// set target speed based on move speed, sprint speed and if sprint is pressed
+		// float targetSpeed = input.sprint ? SprintSpeed : MoveSpeed;
+
+		// a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
+
+		// note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
+		// if there is no input, set the target speed to 0
+		// if (input.move == Vector2.zero) targetSpeed = 0.0f;
+
+		// a reference to the players current horizontal velocity
+		// float inputMagnitude = input.analogMovement ? input.move.magnitude : 9f;
+
+		// accelerate or decelerate to target speed
+		// if (currentHorizontalSpeed < targetSpeed - speedOffset || currentHorizontalSpeed > targetSpeed + speedOffset)
+		// {
+		// 	// creates curved result rather than a linear one giving a more organic speed change
+		// 	// note T in Lerp is clamped, so we don't need to clamp our speed
+		// 	// _speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude, Time.deltaTime * SpeedChangeRate);
+
+		// 	// round speed to 3 decimal places
+		// 	_speed = Mathf.Round(_speed * 1000f) / 1000f;
+		// }
+		// else
+		// {
+		// 	_speed = targetSpeed;
+		// }
+		// _animationBlend = Mathf.Lerp(_animationBlend, targetSpeed, Time.deltaTime * SpeedChangeRate);
+
+		// normalise input direction
+		// Vector3 inputDirection = new Vector3(input.move.x, 0.0f, input.move.y).normalized;
+
+		// note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
+		// if there is a move input rotate player when the player is moving
+		// if (input.move != Vector2.zero)
+		// {
+		// 	_targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + mainCamera.transform.eulerAngles.y;
+		// 	float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity, RotationSmoothTime);
+
+		// 	// rotate to face input direction relative to camera position
+		// 	transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+		// }
+
+
+		// Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
+
+		// move the player
+		// controller.Move(targetDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+
+		// update animator if using character
+	}
 }
