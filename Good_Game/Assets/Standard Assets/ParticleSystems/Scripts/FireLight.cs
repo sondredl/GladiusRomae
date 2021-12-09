2595ed3eb4fd379b343e61c0d745e78bd24d815b
Petter Tveit <petter.tveit@lyse.net>
2021-12-09 14:59:34 +0100

    Added night scene + lights

--- Good_Game/Assets/Standard Assets/ParticleSystems/Scripts/FireLight.cs
+++ Good_Game/Assets/Standard Assets/ParticleSystems/Scripts/FireLight.cs
@@ -26,7 +26,7 @@ namespace UnityStandardAssets.Effects
                 float x = Mathf.PerlinNoise(m_Rnd + 0 + Time.time*2, m_Rnd + 1 + Time.time*2) - 0.5f;
                 float y = Mathf.PerlinNoise(m_Rnd + 2 + Time.time*2, m_Rnd + 3 + Time.time*2) - 0.5f;
                 float z = Mathf.PerlinNoise(m_Rnd + 4 + Time.time*2, m_Rnd + 5 + Time.time*2) - 0.5f;
-                transform.localPosition = Vector3.up + new Vector3(x, y, z)*1;
+                transform.localPosition = Vector3.up + new Vector3(x, y, z)*10;
             }
         }
 
