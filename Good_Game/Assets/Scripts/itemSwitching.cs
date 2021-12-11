c3db5b8ccca2eb0e3f803a0bc31ee9fd910b4686
Sondre Lindkjølen <47851430+sondredl@users.noreply.github.com>
2021-12-11 23:38:27 +0100

    enable weapon toggle with R

--- Good_Game/Assets/Scripts/itemSwitching.cs
+++ Good_Game/Assets/Scripts/itemSwitching.cs
@@ -11,24 +11,32 @@ public class itemSwitching : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetAxis("Mouse ScrollWheel") > 0f) {
+        Debug.Log("itemSwitching.update()");
+        // if (Input.GetAxis("Mouse ScrollWheel") > 0f) {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            Debug.Log("itemSwitching.update() keykode: R");
             if (selectedItem >= transform.childCount - 1) {
+                Debug.Log("itemSwitching.update() if --> if ");
                 selectedItem = 0;
+                // SelectWeapon();
             } else {
                 selectedItem++;
+                SelectWeapon();
             }
         }
-        if (Input.GetAxis("Mouse ScrollWheel") < 0f) {
-            if (selectedItem <= 0) {
-                selectedItem = transform.childCount - 1;
-            } else {
-                selectedItem--;
-            }
-        }
+        // if (Input.GetAxis("Mouse ScrollWheel") < 0f) {
+        // if (Input.GetKeyDown(KeyCode.R)) {
+        //     if (selectedItem <= 0) {
+        //         selectedItem = transform.childCount - 1;
+        //     } else {
+        //         selectedItem--;
+        //     }
+        // }
     }
 
     void SelectWeapon() {
         int i = 0;
+        Debug.Log("itemSwitching.SelectedWeapon()");
         foreach (Transform item in transform) {
             if (i == selectedItem) {
                 item.gameObject.SetActive(true);
